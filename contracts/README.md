7fd510d9b1710485d43385a17247b66a0556618e
evercreativedev <evercreativedev@gmail.com>
2022-08-24 17:37:19 +0800

    feat: updated git root

---
+++ contracts/README.md
@@ -0,0 +1,18 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+GAS_REPORT=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+
+
+Celo NFT deployed to: 0xef3327233967bBE6760c21F4A1A6373446e8fb34
+NFT Marketplace deployed to: 0xc71f75A909961525B41dDC5417Fa6CEBEe7923B1
---
+++ contracts/scripts/deploy.js
@@ -0,0 +1,40 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  // Load the NFT contract artifacts
+  const CeloNFTFactory = await ethers.getContractFactory("CeloNFT");
+
+  // Deploy the contract
+  const celoNftContract = await CeloNFTFactory.deploy();
+  await celoNftContract.deployed();
+
+  // Print the address of the NFT contract
+  console.log("Celo NFT deployed to:", celoNftContract.address);
+
+  // Load the marketplace contract artifacts
+  const NFTMarketplaceFactory = await ethers.getContractFactory(
+    "NFTMarketplace"
+  );
+
+  // Deploy the contract
+  const nftMarketplaceContract = await NFTMarketplaceFactory.deploy();
+
+  // Wait for deployment to finish
+  await nftMarketplaceContract.deployed();
+
+  // Log the address of the new contract
+  console.log("NFT Marketplace deployed to:", nftMarketplaceContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
