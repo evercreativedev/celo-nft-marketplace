d10b897f12e3f46454027adcaaffa32934d9a810
evercreativedev <evercreativedev@gmail.com>
2022-08-30 15:34:02 +0800

    feat: added marketplace frontend

---
+++ frontend/constants.js
@@ -0,0 +1,6 @@
+export const SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/evercreativedev/celo-nft-marketplace'
+export const MARKETPLACE_ADDRESS = "0xc71f75A909961525B41dDC5417Fa6CEBEe7923B1";
+
+
+
+
